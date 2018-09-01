0c2a4757f2b3e2c3ce92a7c6890f5c0d82ba40cc
Jan Bakalar <jan.bachelor@gmail.com>
2018-09-01 23:19:10 +0200

    Added feature for creating buckets and setting static hosting

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,5 +1,6 @@
 import boto3
 import click
+from botocore.exceptions import ClientError
 
 session = boto3.Session(profile_name='pythonAutomation')
 s3 = session.resource('s3')
@@ -7,13 +8,13 @@ s3 = session.resource('s3')
 @click.group()
 
 def cli():
-    "Webotron deploys websites to AWS"
+    """Webotron deploys websites to AWS"""
     pass
 
 @cli.command('list-buckets')
 
 def list_buckets():
-        "List all s3 buckets"
+        """List all s3 buckets"""
         for bucket in s3.buckets.all():
                 print (bucket)
 
@@ -21,9 +22,57 @@ def list_buckets():
 @click.argument('bucket')
 
 def list_bucket_objects(bucket):
-    "List objects in a s3 bucket"
+    """List objects in a s3 bucket"""
     for obj in s3.Bucket(bucket).objects.all():
         print(obj)
+
+@cli.command('setup-bucket')
+@click.argument('bucket')
+def setup_bucket(bucket):
+    """Create and configure an S3 bucket for static website hosting"""
+    #Clear up variable s3_bucket or else click would complain
+    s3_bucket = None
+    try:
+        s3_bucket = s3.create_bucket(
+            Bucket=bucket,
+            CreateBucketConfiguration={'LocationConstraint': session.region_name}
+        )
+    except ClientError as e:
+        #print(e.response)
+        if e.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
+            s3_bucket = s3.Bucket(bucket)
+        else:
+            raise e
+
+    policy = """
+    {
+        "Version":"2012-10-17",
+        "Statement":[{
+        "Sid":"PublicReadGetObject",
+        "Effect":"Allow",
+        "Principal": "*",
+        "Action":["s3:GetObject"],
+        "Resource":["arn:aws:s3:::%s/*"
+        ]}
+       ]
+     }
+    """ % s3_bucket.name
+    policy = policy.strip()
+
+    pol = s3_bucket.Policy()
+    pol.put(Policy=policy)
+
+    ws = s3_bucket.Website()
+    ws.put(WebsiteConfiguration={
+        'ErrorDocument': {
+            'Key': 'error.html'
+        },
+        'IndexDocument': {
+            'Suffix': 'index.html'
+        }
+    })
+
+    return
 
 if __name__ == '__main__':
         cli()
